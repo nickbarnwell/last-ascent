634404fce2b3cf7206e4ea0de80f04b5b3c68400
Nick Barnwell <nb@ul.io>
2014-12-24 12:55:50 -0800

    Blog configuration

--- config.rb
+++ config.rb
@@ -73,26 +73,26 @@ set :markdown_engine, :redcarpet
 # page "/blog/feed.xml", :layout => false
 # page "/feed.xml", :proxy => "/blog/feed.xml", :layout => false
 
-# activate :blog do |blog|
-#   # blog.prefix = "blog"
-#   # blog.permalink = ":year/:month/:day/:title.html"
-#   # blog.sources = "posts/:year-:month-:day-:title.html"
-#   # blog.taglink = "tags/:tag.html"
-#   # blog.layout = "post"
-#   # blog.summary_separator = /(READMORE)/
-#   # blog.summary_length = 250
-#   # blog.year_link = ":year.html"
-#   # blog.month_link = ":year/:month.html"
-#   # blog.day_link = ":year/:month/:day.html"
-#   # blog.default_extension = ".md"
-# 
-#   # blog.tag_template = "blog/tag.html"
-#   # blog.calendar_template = "blog/calendar.html"
-# 
-#   # blog.paginate = true
-#   # blog.per_page = 10
-#   # blog.page_link = "page/:num"
-# end
+activate :blog do |blog|
+  blog.prefix = "blog"
+  # blog.permalink = ":year/:month/:day/:title.html"
+  blog.sources = "posts/:year-:month-:day-:title.html"
+  # blog.taglink = "tags/:tag.html"
+  blog.layout = "post"
+  blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = ":year.html"
+  # blog.month_link = ":year/:month.html"
+  # blog.day_link = ":year/:month/:day.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "blog/tag.html"
+  blog.calendar_template = "blog/calendar.html"
+
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/:num"
+end
 
 # Build-specific configuration
 set :build_dir, 'tmp'
@@ -108,7 +108,7 @@ configure :build do
   # activate :cache_buster
   
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
   
   # Compress PNGs after build
   # First: gem install middleman-smusher
